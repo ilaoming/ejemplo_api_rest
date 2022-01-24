1ce30ad3ecb5891433cbbf72a0d0715a846acbe7
ilaoming <jesus.rios2609@gmail.com>
2022-01-24 15:03:59 -0500

    Add crossDomain

--- app.js
+++ app.js
@@ -11,10 +11,26 @@ var pool = mysql.createPool({
   database: "heroku_ab3189222e34410",
 });
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
 //mysql://b6175f675c62a8:d1812fb5@us-cdbr-east-05.cleardb.net/heroku_ab3189222e34410?reconnect=true
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(allowCrossDomain)
 
 app.get("/",function (req,res) { 
   const apiData = [
