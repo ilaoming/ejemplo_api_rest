ffd7aaf1e5d5a7485f5081fb81da9e9131e11cec
ilaoming <jesus.rios2609@gmail.com>
2022-01-25 15:40:21 -0500

    Add query connection

--- app.js
+++ app.js
@@ -157,14 +157,17 @@ app.get("/api/product/category/:category_id", function (req, res) {
 app.post("/api/product/", function (req, res) {
 
   pool.getConnection(function (err, connection) {
-    const query = `INSERT INTO product (name,description,price,foto,available,category_id) VALUES (
+    const query = `INSERT INTO product (name,description,price,picture,available,category_id) VALUES (
       ${connection.escape(req.body.name)},
       ${connection.escape(req.body.description)},
       ${connection.escape(req.body.price)},
-      ${connection.escape(req.body.foto)},
+      ${connection.escape(req.body.picture)},
       ${connection.escape(req.body.available)},
       ${connection.escape(req.body.category_id)}
       );`;
+      connection.query(query,function (error,filas,campos) { 
+        console.log(error);
+       })
     connection.release();
   });
 });
